d68d6fa03e08e523fe4a9674de3dba4183d8052b
Dmitriy Sokolov <sda.falcon@gmail.com>
2020-09-14 09:22:43 +0500

    пересоздал проект и сделал первую табличку

---
+++ HF.Web/HF.Web/Data/HFWebContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HF.Web.Models;
+
+namespace HF.Web.Data
+{
+    public class HFWebContext : DbContext
+    {
+        public HFWebContext (DbContextOptions<HFWebContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<HF.Web.Models.Currency> Currency { get; set; }
+    }
+}
